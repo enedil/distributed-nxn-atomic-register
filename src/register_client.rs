f710218a94d46fb1e513c7948a953e01828afd1a
Michał Radwański <enedil@outlook.com>
2023-02-05 02:59:58 +0100

    stubborn broadcasd and link

--- src/register_client.rs
+++ src/register_client.rs
@@ -1,5 +1,12 @@
-use crate::SystemRegisterCommand;
-use std::sync::Arc;
+use tokio::{stream, sync::Mutex};
+
+use crate::{PublicConfiguration, SystemRegisterCommand, SystemCommandHeader};
+use std::{
+    collections::{HashSet, HashMap},
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    str::FromStr,
+    sync::Arc,
+};
 
 #[async_trait::async_trait]
 /// We do not need any public implementation of this trait. It is there for use
@@ -21,4 +28,78 @@ pub struct Send {
     pub cmd: Arc<SystemRegisterCommand>,
     /// Identifier of the target process. Those start at 1.
     pub target: u8,
+}
+
+struct ClientInfo {
+    location: (String, u16),
+    stream: Option<tokio::net::TcpStream>,
+    pending_messages: HashMap<SystemCommandHeader, Arc<SystemRegisterCommand>>,
+}
+
+impl ClientInfo {
+    fn new(location: (String, u16)) -> ClientInfo {
+        ClientInfo {
+            location,
+            stream: None,
+            pending_messages: HashMap::new(),
+        }
+    }
+
+    async fn try_connect(&mut self) {
+        if self.stream.is_none() {
+            let sockopt = tokio::net::TcpSocket::new_v4().ok();
+            if let Some(sock) = sockopt {
+                let ipaddropt = IpAddr::from_str(&self.location.0);
+                if let Ok(ipaddr) = ipaddropt {
+                    let addr = std::net::SocketAddr::new(ipaddr, self.location.1);
+                    self.stream = sock.connect(addr).await.ok();
+                }
+            }
+        }
+    }
+
+    async fn try_send(&mut self) {
+        
+    }
+
+    fn append_message(&mut self, msg: Arc<SystemRegisterCommand>) {
+        if self.pending_messages.contains_key(&msg.header) {
+            self.pending_messages.remove(&msg.header);
+        } else {
+            self.pending_messages.insert(msg.header, msg);
+        }
+    }
+}
+
+pub(crate) struct RegisterClientImpl(Vec<Mutex<ClientInfo>>);
+
+impl RegisterClientImpl {
+    pub(crate) fn new(config: &PublicConfiguration) -> RegisterClientImpl {
+        RegisterClientImpl(
+            config
+                .tcp_locations
+                .iter()
+                .map(|x| Mutex::new(ClientInfo::new(x.clone())))
+                .collect(),
+        )
+    }
+}
+
+#[async_trait::async_trait]
+impl RegisterClient for RegisterClientImpl {
+    async fn send(&self, msg: Send) {
+        let mut client = self.0[msg.target as usize - 1].lock().await;
+        
+        client.try_connect().await;
+        
+    }
+    async fn broadcast(&self, msg: Broadcast) {
+        for i in 1..=self.0.len() {
+            self.send(Send {
+                target: i as u8,
+                cmd: msg.cmd.clone(),
+            })
+            .await;
+        }
+    }
 }
